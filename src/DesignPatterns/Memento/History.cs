b8191f7a9ba5c6a903215c8acf7223eda546b554
Musa Alp <musaa.alp@gmail.com>
2020-07-19 01:37:38 +0300

    feat: Add a sample implementation of memento design pattern

---
+++ src/DesignPatterns/Memento/History.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    public class History
+    {
+        private Stack<EditorState> _stack = new Stack<EditorState>();
+
+        public void Push(EditorState state)
+        {
+            _stack.Push(state);
+        }
+
+        public EditorState Pop()
+        {
+            return _stack.Pop();
+        }
+    }
+}

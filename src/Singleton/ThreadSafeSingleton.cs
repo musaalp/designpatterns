56e0355314900eaed9bde0e80fdf4e1d80fa3eb1
Musa Alp <musaa.alp@gmail.com>
2020-08-26 20:00:11 +0300

    feat: Add sample implementation of singleton design pattern

---
+++ src/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class ThreadSafeSingleton
+    {
+        // Variable is declared to be volatile to ensure that assignment to the instance variable completes before the instance variable can be accessed.
+        private static volatile ThreadSafeSingleton _instance;
+        private static object _syncRoot = new Object();
+
+        private ThreadSafeSingleton()
+        {
+        }
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ThreadSafeSingleton();
+                        }
+
+                        return _instance;
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
